cb793211f2f1df88a4b79f43b4f2b8f310585607
EdemEssang <edemoski.eu@gmail.com>
2022-11-30 04:00:40 -0800

    9. Full creation modification

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- this is script that creates a table second_table in the databasehbtn_0c_0 and adding multiple rows.
+-- Creates and fills a table second_table with attributes id, name and score in my MySQL server with multiple rows.
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT `name` VARCHAR(256) `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "ALEX", 3);
