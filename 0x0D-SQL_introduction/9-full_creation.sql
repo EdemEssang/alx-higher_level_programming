7e72d635a738baf5620ffa80764a911d47264666
EdemEssang <edemoski.eu@gmail.com>
2022-11-30 03:49:45 -0800

    9. Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- this is script that creates a table second_table in the databasehbtn_0c_0 and adding multiple rows.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT `name` VARCHAR(256) `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "ALEX", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bod", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+
